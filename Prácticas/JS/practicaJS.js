fcf15fbd785b983be233f6e70dde3be772d3f398
sofihoj <sofiahojberg@gmail.com>
2022-07-20 16:59:10 -0300

    Add: first array

---
+++ Prácticas/JS/practicaJS.js
@@ -0,0 +1,19 @@
+const nombre = "Sofia Hojberg";
+const age = 28;
+const isSingle = True;
+const nros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const skillers = ["Fabricio", "Gonzalo", "Adán", "Emiliano", "Maria"];
+const objeto = {
+    nombre: "Sofia Hojberg",
+    age: 28,
+    isSingle: True,
+    nros: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    skillers: ["Fabricio", "Gonzalo", "Adán", "Emiliano", "Maria"]
+};
+
+console.log(nombre);
+console.log(age);
+console.log(isSingle);
+console.log(nros);
+console.log(skillers);
+console.log(objeto);
